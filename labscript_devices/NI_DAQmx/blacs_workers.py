79edf971398cfe83804fcdf4e0e52c596a91f1bf
Matthew Peters <mpeters1@mit.edu>
2023-07-14 10:48:22 -0400

    Formatting changes

--- labscript_devices/NI_DAQmx/blacs_workers.py
+++ labscript_devices/NI_DAQmx/blacs_workers.py
@@ -221,8 +221,10 @@ class NI_DAQmxOutputWorker(Worker):
 
             # UPDATE: # Our digital card is old and needs this to be a multiple of 4. I don't fully understand what is being programed here, but it appears to work
 
-            npts = max(2**8, len(DO_table)) # 2**14 is the maximum number of points that can be written I believe. If len(DO_table) > 2**14, this should throw an error
-
+            if self.MAX_name == '6534_A':
+                npts = max(2**8, len(DO_table)) # 2**14 is the maximum number of points that can be written I believe. If len(DO_table) > 2**14, this should throw an error
+            else:
+                npts = len(DO_table) - 1
 
 
             # Set up timing:
@@ -362,10 +364,11 @@ class NI_DAQmxOutputWorker(Worker):
                 if not static:
                     try:
                         # Wait for task completion with a 1 second timeout:
-                        if not self.MAX_name == "DO_6534_A":
+                        if not self.MAX_name == "6534_A":
                             task.WaitUntilTaskDone(1)
                     except Exception as e:
-                        if self.MAX_name == "ni_6534_Dev1":
+
+                        if self.MAX_name == "6534_A":
                             print("This digital card does not need to worry about the fact that the # samples does not reach the max (probably)")
                             # print("Look in the transition_to_buffered method for more info.\n")
                             # print("It appears our 6534 card needs nsamples%4 = 0 and have some weird problems with programming ...\n but as far as I can tell it works like this.")
